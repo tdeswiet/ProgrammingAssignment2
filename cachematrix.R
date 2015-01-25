a337d0de013d30578ba95bf08c5596920cade819
Thomas de Swiet <tdeswiet@gmail.com>
2015-01-25 09:11:52 -0800

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions addresses the situation where you don't want to
+## compute the inverse of a function more than once. If the inverse of a
+## particular matrix has been calculated before it will be pulled from a "cache"
+## where the matrix is also stored.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix spawns an environment (or "cache") containing a 
+## matrix and it's inverse and returns a list of 4 functions that access that
+## environment.
+
+##Because the variables X and i are assigned with the <<- operator,
+## they are searched for until found up the environment tree. In other words the
+## point where the 4 functions are defined upon a particular  call of
+## makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
-  set <- function(y) {
-    x <<- y
+  set <- function(y) {  # sets the matrix in the environment
+    x <<- y             
     i <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
+  get <- function() x   # gets the matrix
+  setinverse <- function(inverse) i <<- inverse 
   getinverse <- function() i
   list(set = set, get = get,
        setinverse = setinverse,
@@ -18,7 +27,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## Write a short comment describing this function
+## This function operates on a cached matrix defined and set by makeCacheMatrix,
+## and computes the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
